d2ac83a82c43046dda450657db7096d583ef70a0
johnfurie <44396278+johnfurie@users.noreply.github.com>
2020-10-13 13:51:28 +0200

    Update Install_cenith_and_legion_standalone_package.R

--- src/Tutorial/Install_cenith_and_legion_standalone_package.R
+++ src/Tutorial/Install_cenith_and_legion_standalone_package.R
@@ -1,3 +1,4 @@
-devtools::install_github("SchoenbergA/CENITH@develop")
+devtools::install_github("SchoenbergA/CENITH@develop", force = T, build_vignettes = T)
 devtools::install_github("SchoenbergA/LEGION@develop")
 
+a <- vignette("CENITH")
